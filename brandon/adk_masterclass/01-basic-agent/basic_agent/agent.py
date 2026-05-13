fbca73f84dd714996045e0733fdec0eb1c03e339
Manish Bhobe <mjbhobe@gmail.com>
2026-05-13 13:15:20 +0000

    Basic Agents using GPT 5 Nano LLM

--- brandon/adk_masterclass/01-basic-agent/basic_agent/agent.py
+++ brandon/adk_masterclass/01-basic-agent/basic_agent/agent.py
@@ -1,15 +1,18 @@
+from dotenv import load_dotenv, find_dotenv
 from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 
-openai_model = LiteLlm(model="openai/gpt-4o")
-sonnet_model = LiteLlm(model=)
+load_dotenv(find_dotenv(), override=True)
+
+openai_model = LiteLlm(model="openai/gpt-5-nano")
 
 root_agent = LlmAgent(
     name="greeting_agent",
     model=openai_model,
     description="Greeting agent",
     instruction="""
-    You are a helpful assistant that greets the user with a "Hi! I am the ADK Agent. How can I help you?"
+    You are a helpful assistant that greets the user with a 
+    "Hi! I am the ADK Agent. How can I help you?"
     Ask for the user's name and then greet them by their name.
     Respond to any questions from your pre-trained data only as you do not have 
     access to any external tools.
